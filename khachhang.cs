eddf48283a7102e4231853ad9d9349bab87848ac
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2021-11-18 21:37:00 +0700

    Create khachhang.cs

---
+++ khachhang.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quan_ly_nhan_su
+{
+    class khachhang
+    {
+        public string makh { get; set; }
+        public string tenkh { get; set; }
+        public string gioitinh { get; set; }
+        public string loaikh { get; set; }
+        public string ngaysinh { get; set; }
+    }
+}
