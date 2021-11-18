c4da2564b7cec9b0b9aa54a547f6006756c2fc02
phamcaosanh <72543595+phamcaosanh@users.noreply.github.com>
2021-11-18 21:37:36 +0700

    Create nhanvien.cs

---
+++ nhanvien.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quan_ly_nhan_su
+{
+    class nhanvien
+    {
+        public string manv { get; set;}
+        public string tennv { get; set; }
+        public string gioitinh { get; set; }
+        public string bangcap { get; set; }
+        public string ngaysinh { get; set; }
+    }
+}
